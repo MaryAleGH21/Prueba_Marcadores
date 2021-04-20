b1595f78014d4e5726885540abb4583fe7aeb2b7
Alejandra <marialesr.17@gmail.com>
2021-04-20 15:53:09 -0600

    created endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :kinds
   resources :categories
   root 'home#index'
-
+  get 'categories/:id/api', to: 'categories#api', as: 'api'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
